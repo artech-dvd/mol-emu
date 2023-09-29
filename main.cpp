16b65f8cb8e1c29ea2aa5111f03db96d1e23579a
HalfHaIf <69714108+HalfHaIf@users.noreply.github.com>
2023-09-30 02:08:49 +1000

    Fixed up some issues with the AND function

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 
 // C++ does not have 24-bit integers, so this thingy allows us to make our own
-struct int24{
-    unsigned int data : 24;
+struct uint24
+{
+     unsigned int  int24 : 24;
 };
 
-bool and_func(int24 x, int24 y) {
+
+bool and_func(int x, int y) {
 	// Returns both of the first bit of X and Y are both true
 	// Sorry for the fancy unreadable math, it's the solution I found to this on StackOverflow
-	return ( ( (x) & ( 1 << 1 )) >> 1) &&  ( ( (y) & ( 1 << 1 )) >> 1);
+	return ( ( x & ( 1 << 0 )) >> 0) &&  ( ( y & ( 1 << 0 )) >> 0);
 }
 int main() {
 	
-	// Set both X and Y to true
-	int24 x = 1;
-	int24 y = 1;
+	// Set both X and Y to 1
+	uint24 x;
+	x.int24 = 1;
+
+	uint24 y;
+	y.int24 = 1;
 
 	// Pass X and Y into our "AND" function and print it
-	std::cout << and_func(x, y) << "/n";
+	std::cout << and_func(x.int24, y.int24) << "\n";
 	return 0;
 }
 
